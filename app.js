592b59cc4b79dad340f8b2d5c95ea8637dac85bb
smd232 <maryamdanial886@gmail.com>
2026-08-16 12:15:17 +0500

    add file

--- app.js
+++ app.js
@@ -98,6 +98,9 @@ app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use((req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
